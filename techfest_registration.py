ebde2730e7ae98f393fa6bd5d0ad7c1260cdd63a
Sean <seanathanscott.ibay@benilde.edu.ph>
2025-11-06 12:25:30 +0800

    Implemented participant registration

--- techfest_registration.py
+++ techfest_registration.py
@@ -6,3 +6,18 @@ try:
 except ValueError:
     print("Invalid input. Please enter a valid number.")
     exit()
+
+if num_participants <= 0:
+    print("Invalid number of participants.")
+    exit()
+
+participants = []
+
+for i in range (num_participants):
+    name = input ("Enter participant name: ").strip()
+    track = input ("Enter chosen track: ").strip()
+    participants.append({"name": name, "track": track})
+
+print("\nRegistered participants:")
+for i, p in enumerate(participants, start=1):
+    print(f"{i}. {p['name']} - {p['track']}")
